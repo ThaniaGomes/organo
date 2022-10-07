e8c6d7b92dad5b4276a9a40908f587c15d364437
Thânia Gomes <79340835+ThaniaGomes@users.noreply.github.com>
2022-10-07 16:28:20 -0300

    inserindo banner

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,9 @@
+
+function Banner{
+    //JSX Como react le e transforma os elementos no DOM.
+    return (
+        <img src="/imagens/banner.png" alt="O banner principal da página do organo"></img>
+    )
+}
+
+export default Banner
