8c49b0dbf140d045d7917c29dbb9ce47d0cb1cb4
Thânia Gomes <79340835+ThaniaGomes@users.noreply.github.com>
2022-10-07 16:39:11 -0300

    finalizando banner

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,8 +1,11 @@
+import './Banner.css'
 
 function Banner(){
     //JSX Como react le e transforma os elementos no DOM.
     return (
+        <header className="banner">
         <img src="/imagens/banner.png" alt="O banner principal da página do organo"></img>
+        </header>
     )
 }
 
