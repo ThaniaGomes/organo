9f76395f5cfb752540ada360cc755e6280e5b8fb
Thânia Gomes <79340835+ThaniaGomes@users.noreply.github.com>
2022-10-17 15:25:45 -0300

    criando botao

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,6 +1,7 @@
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
+import Botao from "../Botao";
 
 const times = [
     'Programação',
@@ -21,6 +22,9 @@ const Formulario = () => {
             <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
             <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
             <ListaSuspensa label="Time"  itens={times}/>
+            <Botao>
+                Criar Card
+            </Botao>
         </form>
     </section>
     )
