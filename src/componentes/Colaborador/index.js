13e7599562d69794d5f7e76cf4b7a444a3c170ee
Thânia Gomes <79340835+ThaniaGomes@users.noreply.github.com>
2022-11-01 15:05:12 -0300

    criando os novos cards

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return(
+    <div className='colaborador'>
+        <div className='cabecalho'> 
+            <img src='http://github.com/ThaniaGomes.png' alt='imagem do colaborador'/>
+        </div>
+
+        <div className='rodape'>
+            <h4>Thânia Gomes</h4>
+            <h5>Front End</h5>
+        </div>
+    </div>
+    )
+}
+
+export default Colaborador
